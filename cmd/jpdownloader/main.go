f8c95b2017bf5fd1927ecb9192a5310da3a2521b
Joseda Rios <jotadrilo@users.noreply.github.com>
2019-09-19 15:38:32 +0200

    Add output directory feature to the jpdownloader (#14)

--- cmd/jpdownloader/main.go
+++ cmd/jpdownloader/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,6 +12,7 @@ import (
 	"github.com/bitnami-labs/jenkins-plugins-resolver/pkg/plugins/jpi"
 	"github.com/bitnami-labs/jenkins-plugins-resolver/pkg/utils"
 	"github.com/juju/errors"
+	"github.com/mmikulicic/multierror"
 )
 
 const (
@@ -17,9 +20,41 @@ const (
 )
 
 var (
-	workingDir = flag.String("working-dir", filepath.Join(os.Getenv("HOME"), ".jenkins"), "plugins working dir")
+	workingDir = flag.String("working-dir", filepath.Join(os.Getenv("HOME"), ".jenkins"), "plugins working directory, default to the HOME/.jenkins directory")
+	outputDir  = flag.String("output-dir", filepath.Join(os.Getenv("JENKINS_HOME"), "plugins"), "output directory, default to the JENKINS_HOME/plugins directory")
 	inputFile  = flag.String("input", "plugins.json.lock", "input file. You can use the output of jpresolver")
 )
+
+// This function will copy the downloaded plugins (from the working directory
+// used to as fs cache) to the output directory. It will rename them to match
+// the jenkins requirements (.pinned suffix and no version in the filename).
+func copyPlugins(plugins *api.PluginsRequest) error {
+	var errs error
+	for _, p := range plugins.Plugins {
+		src := jpi.GetPluginPath(p, *workingDir)
+		r, err := os.Open(src)
+		if err != nil {
+			errs = multierror.Append(errs, errors.Trace(err))
+			continue
+		}
+		defer r.Close()
+
+		dst := filepath.Join(*outputDir, fmt.Sprintf("%s.jpi.pinned", p.Name))
+		w, err := os.Create(dst)
+		if err != nil {
+			errs = multierror.Append(errs, errors.Trace(err))
+			continue
+		}
+		defer w.Close()
+
+		_, err = io.Copy(w, r)
+		if err != nil {
+			errs = multierror.Append(errs, errors.Trace(err))
+			continue
+		}
+	}
+	return errs
+}
 
 func run() error {
 	plugins := &api.PluginsRequest{}
@@ -27,7 +62,11 @@ func run() error {
 		return errors.Trace(err)
 	}
 
-	return jpi.RunWorkersPoll(plugins, *workingDir, maxWorkers)
+	if err := jpi.RunWorkersPoll(plugins, *workingDir, maxWorkers); err != nil {
+		return errors.Trace(err)
+	}
+
+	return copyPlugins(plugins)
 }
 
 func init() {
@@ -35,6 +74,12 @@ func init() {
 	if *inputFile == "" {
 		flag.Usage()
 		log.Fatalf("undefined input file")
+	}
+	if ok, err := utils.FileExists(*outputDir); err != nil {
+		log.Fatalf("%+v", err)
+	} else if !ok {
+		flag.Usage()
+		log.Fatalf("the output directory does not exist")
 	}
 	// Ensure working paths exist
 	if err := os.MkdirAll(*workingDir, 0777); err != nil {
