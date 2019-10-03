35b32c491a21e06e95c6c9f599d748d1f8e86a1f
Joseda Rios <jotadrilo@users.noreply.github.com>
2019-10-03 14:15:14 +0200

    Replace channels sync with waiting group (#33)
    * Replace channels sync with waiting group
    
    * Add mutex to goroutine
    
    * Use channels instead of waiting group + mutex
    
    * Tackle feedback

--- pkg/plugins/war/extractor.go
+++ pkg/plugins/war/extractor.go
@@ -17,6 +17,7 @@ const (
 type manifestResponse struct {
 	Error    error
 	Manifest string
+	Plugin   string
 }
 
 // ExtractDetachedPluginsManifests takes a war and returns a reader to a detached plugin manifest content
@@ -26,38 +27,40 @@ func ExtractDetachedPluginsManifests(war string, detachedPlugins []string) (map[
 		return nil, errors.Trace(err)
 	}
 
-	manifestsChannel := make(map[string]chan manifestResponse, len(detachedPlugins))
+	manifestsChannel := make(chan manifestResponse, len(detachedPlugins))
 	for dp, bytes := range detachedPluginsBytes {
 		go func(dpName string, dpBytes []byte) {
 			// Create temp file for storing the plugin
 			file, err := ioutil.TempFile("", "plugin")
 			if err != nil {
-				manifestsChannel[dpName] <- manifestResponse{Error: errors.Trace(err), Manifest: ""}
+				manifestsChannel <- manifestResponse{Plugin: dpName, Error: errors.Trace(err)}
+				return
 			}
 			defer os.Remove(file.Name())
 
 			if err := ioutil.WriteFile(file.Name(), dpBytes, 0644); err != nil {
-				manifestsChannel[dpName] <- manifestResponse{Error: errors.Trace(err), Manifest: ""}
+				manifestsChannel <- manifestResponse{Plugin: dpName, Error: errors.Trace(err)}
+				return
 			}
 
 			manifest, err := jar.ExtractManifest(file.Name())
 			if err != nil {
-				manifestsChannel[dpName] <- manifestResponse{Error: errors.Trace(err), Manifest: ""}
-
+				manifestsChannel <- manifestResponse{Plugin: dpName, Error: errors.Trace(err)}
+				return
 			}
-			manifestsChannel[dpName] <- manifestResponse{Error: err, Manifest: manifest}
+			manifestsChannel <- manifestResponse{Plugin: dpName, Manifest: manifest}
 		}(dp, bytes)
 	}
 
 	var errs error
 	detachedPluginsManifests := make(map[string]string, len(detachedPlugins))
-	for dp, ch := range manifestsChannel {
-		mr := <-ch
+	for i := 0; i < len(detachedPlugins); i++ {
+		mr := <-manifestsChannel
 		if mr.Error != nil {
-			errs = multierror.Append(errs, errors.Trace(err))
+			errs = multierror.Append(errs, errors.Annotatef(mr.Error, "unable to extract manifest from %s", mr.Plugin))
 			continue
 		}
-		detachedPluginsManifests[dp] = mr.Manifest
+		detachedPluginsManifests[mr.Plugin] = mr.Manifest
 	}
 
 	return detachedPluginsManifests, errs
